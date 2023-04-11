f4690f73f1e3bb530ba4fddff04d0c17f5d4af1b
Shiva yadav <123328731+shubhamyadav12@users.noreply.github.com>
2023-04-12 01:41:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".multiple-text", { 
+    strings: [
+      "Full Stack Developer",
+      "Youtuber",
+      "Blogger"
+    ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
